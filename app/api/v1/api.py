444cf3e4e004335ff177d30ab7ecdc830f5e00e7
AlhajiTaibu <abdurami.taibu@gmail.com>
2026-04-28 15:09:44 +0000

    update and upload_profile avatar api done

--- app/api/v1/api.py
+++ app/api/v1/api.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from app.api.v1.endpoints import auth, job_application, contacts, interview, job_task, documents, notification
+from app.api.v1.endpoints import auth, job_application, contacts, interview, job_task, documents, notification, profile
 
 api_router = APIRouter()
 
@@ -7,6 +7,12 @@ api_router.include_router(
     auth.router,
     prefix="/auth",
     tags=["Authentication"]
+)
+
+api_router.include_router(
+    profile.router,
+    prefix="/profile",
+    tags=["Profile"]
 )
 
 api_router.include_router(
--- app/tasks/document_tasks.py
+++ app/tasks/document_tasks.py
@@ -1,5 +1,3 @@
-import base64
-
 from app.core.celery import celery_app
 from app.core.logging_config import logger
 from app.services.document_service import DocumentService
@@ -7,7 +5,31 @@ from app.services.document_service import DocumentService
 
 @celery_app.task(name="upload_document")
 def upload_document_task(data: dict, file_content: str):
-        document_service = DocumentService()
-        response = document_service.upload_file(data=data, file_content=file_content)
+    document_service = DocumentService("resumes")
+    response = document_service.upload_file(data=data, file_content=file_content)
+    logger.info(f"response: {response}")
+    return response
+
+
+@celery_app.task()
+def upload_image_task(data: dict, file_content: str):
+    try:
+        document_service = DocumentService("images")
+        response = document_service.upload_image(data=data, file_content=file_content)
         logger.info(f"response: {response}")
         return response
+    except Exception as error:
+        logger.error(error)
+        raise Exception(str(error))
+
+
+@celery_app.task()
+def delete_image_task(file_key: str):
+    try:
+        document_service = DocumentService("images")
+        response = document_service.delete_image(file_key=file_key)
+        logger.info(f"response: {response}")
+        return response
+    except Exception as error:
+        logger.error(error)
+        raise Exception(str(error))
