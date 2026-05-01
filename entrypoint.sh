48ca0d3391a4026d8fcb4c17fe855d3e4a40d9a6
AlhajiTaibu <abdurami.taibu@gmail.com>
2026-05-01 00:51:59 +0000

    changes made to Dockerfile and entrypoint.sh to suit hf spaces

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,7 @@ echo "Applying database migrations..."
 alembic -c app/alembic.ini upgrade head
 
 echo "Starting FastAPI..."
-uvicorn app.main:app --host 0.0.0.0 --port 7860 --reload &
+uvicorn app.main:app --host 0.0.0.0 --port 7860 --reload --proxy-headers --forwarded-allow-ips='*' &
 
 echo "Starting Celery Worker..."
 celery -A app.core.celery:celery_app worker --loglevel=info --concurrency=1 --pool=threads &
