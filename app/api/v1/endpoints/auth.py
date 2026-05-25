c28ec42456a09135d2c030eb979e12dd6f286883
AlhajiTaibu <abdurami.taibu@gmail.com>
2026-05-25 18:08:29 +0000

    removed debug logs

--- app/api/v1/endpoints/auth.py
+++ app/api/v1/endpoints/auth.py
@@ -158,10 +158,7 @@ async def google_login():
     """Redirects the user to the Google OAuth2 login page."""
     try:
         async with google_sso:
-            redirect_response = await google_sso.get_login_redirect(redirect_uri=settings.oauth2_redirect_uri)
-            target_google_url = redirect_response.headers.get("location")
-            logger.info(f"👉 TARGET GOOGLE URL BEING SENT: {target_google_url}")
-        return redirect_response
+            return await google_sso.get_login_redirect(redirect_uri=settings.oauth2_redirect_uri)
     except Exception as error:
         logger.error(str(error))
         raise HTTPException(status_code=400, detail=f"An error occurred: {str(error)}")
